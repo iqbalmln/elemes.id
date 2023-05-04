96aef76d29dfed0184d19d33fd1e3a0d1fa4ea63
iqbalmln <iqbalmaulana15784@gmail.com>
2023-05-04 14:14:38 +0700

    feat: create base component button

---
+++ components/base/Button.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+type Color = "green" | "gray" 
+
+interface ButtonProps {
+  title: string;
+  color: Color;
+}
+
+defineProps<ButtonProps>()
+
+</script>
+
+<template>
+  <button
+    :class="color === 'green' ? 'bg-green-primary text-white' : 'bg-[#F2F2F2] text-black'"
+    class="px-[18px] py-[10px] rounded-full">{{ title }}</button>
+</template>
