4c588e3513f579b1363dd75457460eed3ae4132e
iqbalmln <iqbalmaulana15784@gmail.com>
2023-05-04 15:35:43 +0700

    build: integrate  nuxt-icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,13 +12,14 @@ export default defineNuxtConfig({
         },
         {
           rel: "stylesheet",
-          href: "https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,500;0,600;0,700;1,500;1,600;1,700&display=swap",
+          href: "https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap",
         },
       ],
     },
   },
   modules: [
     '@nuxtjs/tailwindcss',
+    'nuxt-icon'
   ],
   tailwindcss: {
     cssPath: "~/assets/css/tailwind.css",
