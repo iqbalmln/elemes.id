ee4ad8ed6cc389e13e8ecd3e6c20fcd049e4adc3
iqbalmln <iqbalmaulana15784@gmail.com>
2023-05-04 14:14:47 +0700

    feat: create tag component button

---
+++ components/base/Tag.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+interface TagProps {
+  title: string;
+}
+
+defineProps<TagProps>()
+
+</script>
+
+<template>
+  <span class="bg-red-500 text-white text-[8px] uppercase px-2 py-[2px] rounded-lg">{{ title }}</span>
+</template>
