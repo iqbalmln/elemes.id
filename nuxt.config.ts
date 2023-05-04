e5b00b438c6be313d1f9bfd3c5ded1db3252726e
iqbalmln <iqbalmaulana15784@gmail.com>
2023-05-04 13:23:38 +0700

    chore: setup font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: "Elemes.id",
+      link: [
+        { rel: "preconnect", href: "https://fonts.googleapis.com" },
+        {
+          rel: "preconnect",
+          href: "https://fonts.gstatic.com",
+          crossorigin: "anonymous",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,500;0,600;0,700;1,500;1,600;1,700&display=swap",
+        },
+      ],
+    },
+  },
   modules: [
     '@nuxtjs/tailwindcss',
   ],
