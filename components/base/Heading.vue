c3566e7d288039c32e9756176a3f878d79f64e9f
iqbalmln <iqbalmaulana15784@gmail.com>
2023-05-04 15:38:07 +0700

    feat: create heading base component

---
+++ components/base/Heading.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+interface HeadingProps {
+  title: string;
+  subtitle: string;
+}
+
+defineProps<HeadingProps>()
+
+</script>
+
+<template>
+  <header class="flex flex-col gap-2 text-[38px]">
+    <h2>{{ title }}</h2>
+    <p class="text-green-primary">{{ subtitle }}</p>
+  </header>  
+</template>
