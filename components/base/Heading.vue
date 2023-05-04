68e8e55455c7290ce3f253b66954dd64f292f96e
iqbalmln <iqbalmaulana15784@gmail.com>
2023-05-05 02:48:40 +0700

    fix: gap for heading

--- components/base/Heading.vue
+++ components/base/Heading.vue
@@ -9,7 +9,7 @@ defineProps<HeadingProps>()
 </script>
 
 <template>
-  <header class="flex flex-col gap-2 text-[38px]">
+  <header class="flex flex-col text-[38px]">
     <h2>{{ title }}</h2>
     <p class="text-green-primary">{{ subtitle }}</p>
   </header>  
