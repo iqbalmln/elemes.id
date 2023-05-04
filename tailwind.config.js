adf3e7d64f94b874199d7a6fdedf382130876c9d
iqbalmln <iqbalmaulana15784@gmail.com>
2023-05-04 13:24:22 +0700

    chore: setup colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{vue,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'green-primary': '#8BAC3E',
+        'green-secondary': '#F0FEEB',
+        'green-tertiary': '#F9FFF6',
+        'sky-primary': '#40A2B1',
+        'sky-secondary': '#E6F3F5',
+        'blue-primary': '#405EB6',
+        'blue-secondary': '#EAEEFA',
+        'pink-primary': '#B23F74',
+        'pink-secondary': '#F9EEF3',
+        'pistachio-primary': '#A4B441',
+        'pistachio-secondary': '#F3F7D9',
+      }
+    },
   },
   plugins: [],
 }
