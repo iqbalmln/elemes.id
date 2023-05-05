ba974a1fd9d382e97a92cf03e985e9820467ce84
iqbalmln <iqbalmaulana15784@gmail.com>
2023-05-05 14:04:50 +0700

    style: responsive glass card base component

--- components/base/GlassCard.vue
+++ components/base/GlassCard.vue
@@ -29,21 +29,21 @@ const stars = ref([
 </script>
 
 <template>
-  <div class="tes flex gap-5 p-5 w-[350px]">
-    <figure class="w-[75px] h-auto">
+  <div class="glass flex items-center gap-5 p-3 lg:p-5 w-[300px] lg:w-[350px]">
+    <figure class="w-[50px] h-[50px] lg:w-[75px] lg:h-auto">
       <img src="~/assets/image/hero-image.png" class="w-full h-full object-cover" alt="">
     </figure>
     <div class="flex flex-col">
       <p class="font-semibold">Green Salad Tomato</p>
-      <span class=text-[#757575]>Tomato</span>
+      <span class="text-[#757575] font-normal">Tomato</span>
       <div>
-        <Icon v-for="star in stars" :name="star.icon" :color="star.active ? 'orange' : 'gray'" />
+        <Icon v-for="star in stars" :name="star.icon" :color="star.active ? 'orange' : 'gray'" class="text-xs" />
       </div>
     </div>
   </div>
 </template>
 <style scoped>
-.tes {
+.glass {
 background: rgba(255, 255, 255, 0.22);
 border-radius: 16px;
 box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
