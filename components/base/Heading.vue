046cd1db799e2022982cc16a833a2afa7efe8aad
iqbalmln <iqbalmaulana15784@gmail.com>
2023-05-05 14:05:04 +0700

    style: responsive heading base component

--- components/base/Heading.vue
+++ components/base/Heading.vue
@@ -9,7 +9,7 @@ defineProps<HeadingProps>()
 </script>
 
 <template>
-  <header class="flex flex-col text-[38px]">
+  <header class="flex flex-col text-2xl lg:text-[38px]">
     <h2>{{ title }}</h2>
     <p class="text-green-primary">{{ subtitle }}</p>
   </header>  
